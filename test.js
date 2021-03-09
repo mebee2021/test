05a3ecad6c39d82d47dd449f26c81f433ef8d4c6
Unknown <info@mebee.info>
2021-03-09 13:30:03 +0900

    test.jsポート変更

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const http = require('http');
 http.createServer(
   function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write('Hello World!');
+    res.write('Hello node.js!');
     res.end();
   }
-).listen(81,"0.0.0.0");
+).listen(80,"0.0.0.0");
